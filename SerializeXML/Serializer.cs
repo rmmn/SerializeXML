4e617de2bcb12d0dc86de7262a0d32153cbff890
rmmn <43585084+rmmn@users.noreply.github.com>
2020-06-05 18:38:20 +0300

    Create

--- SerializeXML/Serializer.cs
+++ SerializeXML/Serializer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.ComTypes;
+﻿using System.IO;
 using System.Xml.Serialization;
 
 namespace SerializeXML
@@ -11,41 +7,59 @@ namespace SerializeXML
     {
         private T model { get; set; }
         private string path { get; set; }
-
         private string filename { get; set; }
 
+        private XmlSerializer xmlSerializer { get; set; }
+
+        /// <summary>
+        /// Конструктор класса с настройками для сериализации
+        /// </summary>
+        /// <param name="filename">Имя XML файла</param>
+        /// <param name="model">Класс для сериализации</param>
         public Serializer(string filename, T model)
         {
             this.path = Directory.GetCurrentDirectory();
             this.filename = filename;
             this.model = model;
+
+            this.xmlSerializer = new XmlSerializer(typeof(T));
         }
 
+        /// <summary>
+        /// Конструктор класса с настройками для сериализации
+        /// </summary>
+        /// <param name="path">Путь сохранения сериализованного файла, по умолчанию - текущая директория</param>
+        /// <param name="filename">Имя XML файла</param>
+        /// <param name="model">Класс для сериализации</param>
         public Serializer(string path, string filename, T model)
         {
             this.path = path;
             this.filename = filename;
             this.model = model;
+
+            this.xmlSerializer = new XmlSerializer(typeof(T));
         }
 
         public void Serialize()
         {
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
             char sep = Path.DirectorySeparatorChar;
             using (FileStream fs = new FileStream($"{this.path}{sep}{this.filename}.xml", FileMode.OpenOrCreate))
             {
-                xmlSerializer.Serialize(fs, model);
+                this.xmlSerializer.Serialize(fs, model);
             }
         }
 
+        /// <summary>
+        //// Десериализация объекта
+        /// </summary>
+        /// <returns>Класс </returns>
         public T Deserialize()
         {
             T result;
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
             char sep = Path.DirectorySeparatorChar;
             using (FileStream fs = new FileStream($"{this.path}{sep}{this.filename}.xml", FileMode.OpenOrCreate))
             {
-                result = (T)xmlSerializer.Deserialize(fs);
+                result = (T)this.xmlSerializer.Deserialize(fs);
             }
 
             return result;
